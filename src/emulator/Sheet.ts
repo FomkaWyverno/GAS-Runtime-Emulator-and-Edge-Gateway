f021db206bd4fb15e147126edbae3359a7ea62ce
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2026-05-18 01:19:09 +0300

    getSheetByName

---
+++ src/emulator/Sheet.ts
@@ -0,0 +1,7 @@
+
+export class Sheet {
+    constructor(
+        private readonly sheet_id: number,
+        private readonly sheet_name: string
+    ) {}
+}
--- src/emulator/Spreadsheet.ts
+++ src/emulator/Spreadsheet.ts
@@ -1,6 +1,9 @@
+import Database from "../core/database/Database.js";
+import { Sheet } from "./Sheet.js";
 
 export class Spreadsheet {
 
+    private readonly sheetMap: Map<string, Sheet> = new Map();
 
     constructor(
         private readonly id: string
@@ -14,5 +17,28 @@ export class Spreadsheet {
      */
     public getId(): string {
         return this.id;
+    }
+
+    /**
+     * Returns a sheet with the given name.
+     * If multiple sheets have the same name, the leftmost one is returned.
+     * Returns null if there is no sheet with the given name.
+     * @param name 
+     * @returns 
+     */
+    public getSheetByName(name: string): Sheet | null {
+        const cacheSheet = this.sheetMap.get(name);
+        if (cacheSheet) return cacheSheet;
+
+        const sql = 'SELECT `sheet_id`, `name` WHERE `spreadsheet_id` = ? AND `name` = ? LIMIT 1;';
+        const result = Database.query<{sheet_id: number, name: string}>(sql, [this.getId(), name]);
+
+        if (result[0]) {
+            const sheet = new Sheet(result[0].sheet_id, result[0].name);
+            this.sheetMap.set(name, sheet);
+            return sheet;
+        } 
+
+        return null;
     }
 }
