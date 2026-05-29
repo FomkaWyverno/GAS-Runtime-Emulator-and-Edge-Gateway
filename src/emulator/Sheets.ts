940a5e059699d6337513eda76ed87dcdc272b526
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2026-05-29 15:00:20 +0300

    start implementation SpreadsheetsCollection.batchUpdate

---
+++ src/emulator/Sheets.ts
@@ -0,0 +1,7 @@
+import { SpreadsheetsCollection } from "./SpreadsheetsCollection.js";
+
+class Sheets {
+    public Spreadsheets = new SpreadsheetsCollection();
+}
+
+export default new Sheets();
---
+++ src/emulator/SpreadsheetsCollection.ts
@@ -0,0 +1,10 @@
+
+export interface SpreadsheetAPIBatchUpdateRequest {
+    requests: 
+} 
+
+export class SpreadsheetsCollection {
+    public batchUpdate(requests: Object, spreadsheetId: string): void {
+
+    }
+}
