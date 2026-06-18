8b772be3bc8943fd3ae53990bf7b586f3ac47b2a
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2026-06-18 19:04:10 +0300

    AppBootstrap add phase

--- src/core/bootstrap/AppBootstrap.ts
+++ src/core/bootstrap/AppBootstrap.ts
@@ -1,12 +1,14 @@
 import { BootStrap } from "./Bootstrap.js";
+import PropertiesDataBootstrap from "./PropertiesDataBootstrap.js";
 import SheetsDataBootstrap from "./SheetsDataBootstrap.js";
 
 
 class AppBootstrap implements BootStrap {
     public async boot() {
-        console.log('[Boot] - Phase 1 | Data sheet sync');
+        console.log('[AppBoot] - Phase 1 | Data sheet sync');
         SheetsDataBootstrap.boot();
-        
+        console.log(`[AppBoot] - Phase 2 | Insert default script properties`);
+        PropertiesDataBootstrap.boot();
     }
 }
 
